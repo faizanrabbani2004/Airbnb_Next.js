8771fb969b718fe15a75eeddd518af433f074fec
faizanrabbani2004 <bscs22067@itu.edu.pk>
2024-11-24 12:14:11 +0500

    App.js files are updated

--- Project/Client/src/App.jsx
+++ Project/Client/src/App.jsx
@@ -11,14 +11,7 @@ function App() {
       <div>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
